4b94cd7d5fe3fb16fca525e19fcf12c127aac2a1
UseK <use.k.james@gmail.com>
2019-08-13 18:19:23 +0900

    Clear ABS 7(Card Game for Two)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let input_vec = read_to_vec::<usize>();
-    let n = input_vec[0];
-    let a = input_vec[1];
-    let b = input_vec[2];
-    println!("{}", resolve(n, a, b));
+    let _n = read::<usize>();
+    let a_list = read_to_vec::<usize>();
+    println!("{}", resolve(a_list));
+}
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
 }
 
 fn read_to_vec<T: std::str::FromStr>() -> Vec<T> {
@@ -12,22 +16,19 @@ fn read_to_vec<T: std::str::FromStr>() -> Vec<T> {
     s.split_whitespace().map(|s| s.parse().ok().unwrap()).collect()
 }
 
-fn resolve(n: usize, a: usize, b: usize) -> usize {
-    let filtered = (1..n+1).filter(|&i| {
-        let sum = sum_in_each_digit(i);
-        sum >= a && sum <= b
-    });
-    filtered.fold(0, |sum, i| sum + i)
-}
-
-fn sum_in_each_digit(i: usize) -> usize {
-    let mut current = i;
-    let mut sum = 0;
-    while current !=0 {
-        sum += current % 10;
-        current = current / 10;
+fn resolve(mut a_list: Vec<usize>) -> usize {
+    a_list.sort();
+    a_list.reverse();
+    let mut alice = 0;
+    let mut bob = 0;
+    for (i, &a) in a_list.iter().enumerate() {
+        if i % 2 == 0 {
+            alice += a;
+        } else {
+            bob += a;
+        }
     }
-    sum
+    alice - bob
 }
 
 
@@ -36,14 +37,8 @@ mod tests {
     use ::{sum_in_each_digit, resolve};
 
     #[test]
-    fn test_sum_in_each_digit() {
-        let sum = sum_in_each_digit(258);
-        assert_eq!(2+5+8, sum);
-    }
-
-    #[test]
     fn test_resolve() {
-        let result = resolve(10, 1, 2);
-        assert_eq!(result, 13)
+        let result = resolve(vec![20, 18 , 2, 18]);
+        assert_eq!(result, 18);
     }
 }
